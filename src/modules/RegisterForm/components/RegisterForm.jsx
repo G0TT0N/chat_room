1776b36eb32b3a383496791cbf0c14e7c311bb99
g0tt0n <gotton92@mail.ru>
2019-09-02 15:43:09 +0300

    user registration block created

--- src/modules/RegisterForm/components/RegisterForm.jsx
+++ src/modules/RegisterForm/components/RegisterForm.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {Form, Icon, Input} from 'antd';
-import {Link} from 'react-router-dom';
+import {Link, Route} from 'react-router-dom';
 import {Block, Button} from "../../../components";
 
 
 class RegisterForm extends Component {
     render() {
+        const success = true;
         return (
             <div>
                 <div className={'auth__top'}>
@@ -13,47 +14,56 @@ class RegisterForm extends Component {
                     <p>Для входа в чат, вам нужно зарегистрироваться</p>
                 </div>
                 <Block>
-                    <Form onSubmit={this.handleSubmit} className="login-form">
-                        <Form.Item>
-                            <Input
-                                prefix={<Icon type="mail" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                                placeholder="E-mail"
-                                size="large"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input
-                                prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                                placeholder="Ваше имя"
-                                size="large"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input
-                                prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                                type="password"
-                                placeholder="Пароль"
-                                size="large"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input
-                                prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                                type="password"
-                                placeholder="Повторите пароль"
-                                size="large"
-                            />
-                        </Form.Item>
+                    {!success
+                        ? (
+                            <Form onSubmit={this.handleSubmit} className="login-form">
+                                <Form.Item>
+                                    <Input
+                                        prefix={<Icon type="mail" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                        placeholder="E-mail"
+                                        size="large"
+                                    />
+                                </Form.Item>
+                                <Form.Item>
+                                    <Input
+                                        prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                        placeholder="Ваше имя"
+                                        size="large"
+                                    />
+                                </Form.Item>
+                                <Form.Item>
+                                    <Input
+                                        prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                        type="password"
+                                        placeholder="Пароль"
+                                        size="large"
+                                    />
+                                </Form.Item>
+                                <Form.Item>
+                                    <Input
+                                        prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                        type="password"
+                                        placeholder="Повторите пароль"
+                                        size="large"
+                                    />
+                                </Form.Item>
 
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit" size={'large'}>
-                                Зарегистрироваться
-                            </Button>
-                        </Form.Item>
-                        <Form.Item>
-                            <Link className={'auth__register__link'} to={'/login'}>Войти в аккаунт</Link>
-                        </Form.Item>
-                    </Form>
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit" size={'large'}>
+                                        Зарегистрироваться
+                                    </Button>
+                                </Form.Item>
+                                <Form.Item>
+                                    <Link className={'auth__register__link'} to={'/login'}>Войти в аккаунт</Link>
+                                </Form.Item>
+                            </Form>)
+                        : <div className={'auth__success-block'}>
+                            <div>
+                                <Icon style={{fontSize:'48px'}} type="exclamation-circle" theme="twoTone" />
+                            </div>
+                            <h2>Подтвердите свой аккаунт</h2>
+                            <p>На вашу почту отправлено письмо со ссылкой на подтверждение аккаунта</p>
+                        </div>}
                 </Block>
             </div>
         )
