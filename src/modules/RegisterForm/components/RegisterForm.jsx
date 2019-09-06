333fede99d0601e69d56df7a7d17f2f7d0e2dcd5
G0TT0N <gotton92@mail.ru>
2019-09-06 19:16:40 +0300

    formic: mail field validation

--- src/modules/RegisterForm/components/RegisterForm.jsx
+++ src/modules/RegisterForm/components/RegisterForm.jsx
@@ -1,74 +1,91 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Form, Icon, Input} from 'antd';
 import {Link} from 'react-router-dom';
 import {Block, Button} from "../../../components";
 
+const success = false;
 
-class RegisterForm extends Component {
-    render() {
-        const success = false;
-        return (
-            <div>
-                <div className={'auth__top'}>
-                    <h2>Регистрация</h2>
-                    <p>Для входа в чат, вам нужно зарегистрироваться</p>
-                </div>
-                <Block>
-                    {!success
-                        ? (
-                            <Form onSubmit={this.handleSubmit} className="login-form">
-                                <Form.Item>
-                                    <Input
-                                        prefix={<Icon type="mail" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                                        placeholder="E-mail"
-                                        size="large"
-                                    />
-                                </Form.Item>
-                                <Form.Item>
-                                    <Input
-                                        prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                                        placeholder="Ваше имя"
-                                        size="large"
-                                    />
-                                </Form.Item>
-                                <Form.Item>
-                                    <Input
-                                        prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                                        type="password"
-                                        placeholder="Пароль"
-                                        size="large"
-                                    />
-                                </Form.Item>
-                                <Form.Item>
-                                    <Input
-                                        prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                                        type="password"
-                                        placeholder="Повторите пароль"
-                                        size="large"
-                                    />
-                                </Form.Item>
-
-                                <Form.Item>
-                                    <Button type="primary" htmlType="submit" size={'large'}>
-                                        Зарегистрироваться
-                                    </Button>
-                                </Form.Item>
-                                <Form.Item>
-                                    <Link className={'auth__register__link'} to={'/login'}>Войти в аккаунт</Link>
-                                </Form.Item>
-                            </Form>)
-                        : <div className={'auth__success-block'}>
-                            <div>
-                                <Icon style={{fontSize:'48px'}} type="exclamation-circle" theme="twoTone" />
-                            </div>
-                            <h2>Подтвердите свой аккаунт</h2>
-                            <p>На вашу почту отправлено письмо со ссылкой на подтверждение аккаунта</p>
-                        </div>}
-                </Block>
+const RegisterForm = (props) => {
+    const {
+        values,
+        touched,
+        errors,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+    } = props;
+    return (
+        <div>
+            <div className={'auth__top'}>
+                <h2>Регистрация</h2>
+                <p>Для входа в чат, вам нужно зарегистрироваться</p>
             </div>
-        )
+            <Block>
+                {!success
+                    ? (
+                        <Form onSubmit={handleSubmit} className="login-form">
+                            <Form.Item
+                                validateStatus={!touched.email ? '' : errors.email ? 'error' : 'success'}
+                                hasFeedback
+                            >
+                                <Input
+                                    id={'email'}
+                                    prefix={<Icon type="mail" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                    placeholder="E-mail"
+                                    size="large"
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                            </Form.Item>
+                            <Form.Item>
+                                <Input
+                                    prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                    placeholder="Ваше имя"
+                                    size="large"
+                                />
+                            </Form.Item>
+                            <Form.Item>
+                                <Input
+                                    prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                    type="password"
+                                    placeholder="Пароль"
+                                    size="large"
+                                />
+                            </Form.Item>
+                            <Form.Item>
+                                <Input
+                                    prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                    type="password"
+                                    placeholder="Повторите пароль"
+                                    size="large"
+                                />
+                            </Form.Item>
 
-    }
+                            <Form.Item>
+                                <Button type="primary"
+                                        htmlType="submit"
+                                        size={'large'}
+                                        onClick={handleSubmit}
+                                >
+                                    Зарегистрироваться
+                                </Button>
+                            </Form.Item>
+                            <Form.Item>
+                                <Link className={'auth__register__link'} to={'/login'}>Войти в аккаунт</Link>
+                            </Form.Item>
+                        </Form>)
+                    : <div className={'auth__success-block'}>
+                        <div>
+                            <Icon style={{fontSize: '48px'}} type="exclamation-circle" theme="twoTone"/>
+                        </div>
+                        <h2>Подтвердите свой аккаунт</h2>
+                        <p>На вашу почту отправлено письмо со ссылкой на подтверждение аккаунта</p>
+                    </div>}
+            </Block>
+        </div>
+    )
+
 }
 
 export default RegisterForm;
