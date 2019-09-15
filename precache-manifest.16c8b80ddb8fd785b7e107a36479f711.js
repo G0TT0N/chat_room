56dbf4669d29315225e47d9d41e8796efe8282f1
G0TT0N <gotton92@mail.ru>
2019-09-15 16:33:57 +0300

    Updates

---
+++ precache-manifest.16c8b80ddb8fd785b7e107a36479f711.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36be2b2bba3b32cf07b375f8b6c21fc3",
+    "url": "/chat_room/index.html"
+  },
+  {
+    "revision": "6749d1a5744e3afccdfc",
+    "url": "/chat_room/static/css/2.c850e9dc.chunk.css"
+  },
+  {
+    "revision": "086351e058700eeb2f9f",
+    "url": "/chat_room/static/css/main.ac26942e.chunk.css"
+  },
+  {
+    "revision": "6749d1a5744e3afccdfc",
+    "url": "/chat_room/static/js/2.3ed38de4.chunk.js"
+  },
+  {
+    "revision": "086351e058700eeb2f9f",
+    "url": "/chat_room/static/js/main.d36eb475.chunk.js"
+  },
+  {
+    "revision": "8a1b73ac13360bc98d61",
+    "url": "/chat_room/static/js/runtime~main.df6601ff.js"
+  },
+  {
+    "revision": "3c841bf605a8a476802d71028127e33b",
+    "url": "/chat_room/static/media/noreaded.3c841bf6.svg"
+  },
+  {
+    "revision": "a61933e8724eb3f6190514bdd30baadc",
+    "url": "/chat_room/static/media/pause.a61933e8.svg"
+  },
+  {
+    "revision": "3a10db5ca97fb3d2324d74436394cbb2",
+    "url": "/chat_room/static/media/play.3a10db5c.svg"
+  },
+  {
+    "revision": "c1f419d6abb1c32f97508b61040b2074",
+    "url": "/chat_room/static/media/readed.c1f419d6.svg"
+  },
+  {
+    "revision": "2b8196a51fd641948d8c31ba25b52db0",
+    "url": "/chat_room/static/media/wave.2b8196a5.svg"
+  }
+]);
